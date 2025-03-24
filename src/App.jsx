43fd62086a05a48311436d951f67d4dad96770e4
Betelgeuse <b.telgeusedev@gmail.com>
2025-03-24 11:33:16 +0100

    fixing pages error 6

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Landing from './pages/Landing'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+import { Box } from '@chakra-ui/react'
+
+import './App.css'
+
+function App() {
+  return (
+    <Box w="100vw" h="100vh">
+      <Navbar/>
+      <Landing/>
+      <Footer/>
+
+      <div class="blob-outer-container">
+        <div class="blob-inner-container">
+          <div class="blob blob-purple"></div>
+          <div class="blob blob-red"></div>
+          <div class="blob blob-yellow"></div>
+        </div>
+      </div>
+    </Box>
+  )
+}
+
+export default App
