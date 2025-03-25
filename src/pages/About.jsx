b6159f86c51b1ce9f3e8a4cc9946e28912aa0e65
Betelgeuse <b.telgeusedev@gmail.com>
2025-03-25 06:21:28 +0100

    started about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Flex, Box } from "@chakra-ui/react"
+
+function About() {
+  return (
+    <Flex w="100vw" gap="4">
+        <Box>
+
+        </Box>
+
+        <Box>
+
+        </Box>
+        
+        <Box>
+
+        </Box>
+    </Flex>
+  )
+}
+
+export default About
