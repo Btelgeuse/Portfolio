243c8aedebc51b8ddf26c6ed7ccd4913afdb10b9
Betelgeuse <b.telgeusedev@gmail.com>
2025-08-27 23:48:25 +0200

    back to blank page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -11,39 +11,8 @@ import {React, useEffect} from 'react'
 
 function Landing() {
 
-  // const button = document.getElementById('btn');
-
-  useEffect(() => {
-    document.documentElement.style.setProperty('--x', '50vw');
-    document.documentElement.style.setProperty('--y', '50vh');
-
-    const handleMouseMove = (e) => {
-      document.documentElement.style.setProperty('--x', `${e.clientX}px`);
-      document.documentElement.style.setProperty('--y', `${e.clientY}px`);
-    }
-
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    }
-  }, []);
-
   return (
     <Flex>
-      <Container className='bg-container'>
-        <Image src="./landing-background.png"></Image>
-      </Container>
-      
-      <Flex className='overlay'></Flex>
-
-      <Flex className='title-container'>
-        <Heading className='title'>Joanne Andriamahandry</Heading>
-        <Text className='description'>computer science and design.</Text>
-        <Link>
-          <Box id='btn'>enter</Box>
-        </Link>
-      </Flex>
     </Flex>
   )
 }
